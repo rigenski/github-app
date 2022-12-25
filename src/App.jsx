f38d86d4e291462714aa75a50546c439afaa3863
Rigen Maulana <rygen.dev@gmail.com>
2022-12-25 11:32:54 +0700

    feat: added ui mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ function App() {
   return (
     <>
       <main>
-        <div className="container mx-auto py-16">
+        <div className="container mx-auto px-4 py-16">
           <div className="flex flex-col items-start">
-            <div className="mb-4 relative">
+            <h2 className="mb-4 mx-auto text-3xl font-semibold text-slate-800 text-center lg:mx-0 md:text-start">
+              Github Repo
+            </h2>
+            <div className="mb-4 mx-auto relative lg:mx-0">
               <input
                 type="text"
-                className="pl-4 pr-6 py-1.5 w-64 text-sm text-slate-800 border-2 border-slate-200 rounded-md"
+                className="pl-4 pr-6 py-1.5 w-72 text-sm text-slate-800 border-2 border-slate-200 rounded-md"
                 placeholder="Search User"
               />
               <Icon
@@ -19,18 +22,18 @@ function App() {
                 className="text-slate-400 absolute right-4 top-2"
               />
             </div>
-            <div className="mb-2 w-full">
+            <div className="w-72 mb-4 mx-auto lg:mx-0 md:w-auto">
               <div className="p-4 bg-white rounded-md shadow-sm">
-                <div className="flex">
-                  <div className="flex">
-                    <div className="mr-8">
+                <div className="flex flex-col lg:flex-row">
+                  <div className="flex flex-col md:flex-row">
+                    <div className="mx-auto mb-4 w-32 md:mr-8 md:mx-0 md:mb-0">
                       <img
                         src="https://avatars.githubusercontent.com/u/63197023?v=4"
                         alt=""
                         className="w-32 h-32 rounded-full"
                       />
                     </div>
-                    <div className="mr-16">
+                    <div className="mr-0 mb-4 w-40 md:mr-8 md:mb-0">
                       <div className="mb-1">
                         <p className="text-xs font-normal text-slate-400">
                           Full Name
@@ -56,7 +59,7 @@ function App() {
                         </h6>
                       </div>
                     </div>
-                    <div className="mr-16">
+                    <div className="mr-0 mb-4 w-56 md:mr-8 md:mb-0">
                       <p className="mb-4 text-xs font-normal text-slate-400">
                         Detail
                       </p>
@@ -95,18 +98,18 @@ function App() {
                       </div>
                     </div>
                   </div>
-                  <div className="flex justify-center mx-auto">
-                    <div className="flex justify-between items-center">
-                      <div className="mx-8">
-                        <h2 className="text-4xl font-medium text-slate-800 text-center">
+                  <div className="flex mx-auto">
+                    <div className="mr-0 flex justify-between items-center lg:mr-8">
+                      <div className="mr-4">
+                        <h2 className="text-3xl font-medium text-slate-800 text-center">
                           24
                         </h2>
                         <p className="text-xs font-normal text-slate-400 text-center">
                           Followers
                         </p>
                       </div>
-                      <div className="mx-8">
-                        <h2 className="text-4xl font-medium text-slate-800 text-center">
+                      <div className="ml-4">
+                        <h2 className="text-3xl font-medium text-slate-800 text-center">
                           16
                         </h2>
                         <p className="text-xs font-normal text-slate-400 text-center">
@@ -118,8 +121,8 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="w-full">
-              <div className="flex justify-between">
+            <div className="mx-auto w-auto md:mx-0 md:w-full">
+              <div className="hidden justify-between lg:flex">
                 <div className="w-72 px-6 py-2">
                   <p className="text-sm font-normal text-slate-400">NAME</p>
                 </div>
@@ -136,17 +139,17 @@ function App() {
                   <p className="text-sm font-normal text-slate-400">STAR</p>
                 </div>
               </div>
-              <div className="mb-2 flex justify-between items-center bg-white rounded-md shadow-sm">
-                <div className="w-72 px-6 py-4 text-sm text-slate-800 text-left">
+              <div className="mb-2 p-4 w-72 flex flex-wrap justify-between items-start bg-white rounded-md shadow-sm md:p-0 md:w-full md:flex-nowrap md:items-center">
+                <div className="w-full mb-2 p-0 text-sm text-slate-800 text-left md:w-72 md:mb-0 md:px-6 md:py-4">
                   <div className="flex items-center">
                     <Icon
                       icon="ion:document"
                       width={20}
                       height={20}
-                      className="text-slate-400 m-2"
+                      className="text-slate-400 ml-0 mr-2 my-2 md:m-2"
                     />
                     <div className="ml-2">
-                      <h6 className="mb-0.5 text-sm font-medium text-slate-800">
+                      <h6 className="mb-0.5 text-sm font-medium text-slate-800 whitespace-nowrap">
                         Lorem Ipsum
                       </h6>
                       <div className="flex items-center">
@@ -156,27 +159,41 @@ function App() {
                           height={16}
                           className="text-slate-400 mr-2"
                         />
-                        <p className="text-xs font-normal text-slate-600">
+                        <p className="text-xs font-normal text-slate-600 whitespace-nowrap">
                           reactjs, firebase
                         </p>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="w-24 px-6 py-4 text-left">
-                  <p className="text-sm font-normal text-slate-800">4</p>
+                <div className="w-6/12 mb-2 p-0 text-left md:w-24 md:mb-0 md:px-6 md:py-4">
+                  <p className="block text-xs font-normal text-slate-400 md:hidden">
+                    Fork
+                  </p>
+                  <p className="text-sm font-normal text-slate-800 whitespace-nowrap">
+                    4
+                  </p>
                 </div>
-                <div className="w-48 px-6 py-4 text-left">
-                  <p className="text-sm font-normal text-slate-800">
+                <div className="w-6/12 mb-2 p-0 text-left md:w-48 md:mb-0 md:px-6 md:py-4">
+                  <p className="block text-xs font-normal text-slate-400 md:hidden">
+                    Language
+                  </p>
+                  <p className="text-sm font-normal text-slate-800 whitespace-nowrap">
                     Javascript
                   </p>
                 </div>
-                <div className="w-36 px-6 py-4 text-left">
-                  <p className="text-sm font-normal text-slate-800">
+                <div className="w-6/12 p-0 text-left md:w-36 md:px-6 md:py-4">
+                  <p className="block text-xs font-normal text-slate-400 md:hidden">
+                    Date
+                  </p>
+                  <p className="text-sm font-normal text-slate-800 whitespace-nowrap">
                     22-02-2022
                   </p>
                 </div>
-                <div className="w-24 px-6 py-4 text-left">
+                <div className="w-6/12 p-0 text-left md:w-24 md:px-6 md:py-4">
+                  <p className="block text-xs font-normal text-slate-400 md:hidden">
+                    Star
+                  </p>
                   <div className="flex items-center">
                     <Icon
                       icon="ion:star"
@@ -184,7 +201,9 @@ function App() {
                       height={16}
                       className="text-yellow-400 mr-2"
                     />
-                    <p className="text-sm font-normal text-slate-800">2</p>
+                    <p className="text-sm font-normal text-slate-800 whitespace-nowrap">
+                      2
+                    </p>
                   </div>
                 </div>
               </div>
